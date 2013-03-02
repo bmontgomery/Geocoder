523ffcbda19d91494a5b51f957b101b83d4efdf3
Brandon Montgomery <bmonty@gmail.com>
2013-03-02 07:30:34 -0500

    initial commit

---
+++ GeocodeService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Geocoder {
+  public class GeocodeService {
+    private WebClient _client;
+
+    public GeocodeService() {
+      _client = new WebClient();
+    }
+
+    public Location GeocodeLocation(string address) {
+      var url = "http://maps.googleapis.com/maps/api/geocode/xml?sensor=false&address=" + System.Web.HttpUtility.UrlEncode(address);
+
+      var xmlString = _client.DownloadString(url);
+      var xmlDoc = new System.Xml.XmlDocument();
+      xmlDoc.LoadXml(xmlString);
+
+      var loc = new Location();
+      loc.Latitude = Double.Parse(xmlDoc.SelectSingleNode("//geometry/location/lat").InnerText);
+      loc.Longitude = double.Parse(xmlDoc.SelectSingleNode("//geometry/location/lng").InnerText);
+
+      return loc;
+    }
+
+    //public Task GeocodeLocationAsync(string address) {
+    //  return null;
+    //}
+  }
+}
